2de51b080e77691f14d9117899a1f4eec72bddde
paolajfcw <67068066+paolajfcw@users.noreply.github.com>
2020-10-12 22:09:15 +0200

    Updated Search Engine

--- src/Weather.js
+++ src/Weather.js
@@ -5,17 +5,16 @@ import "./Weather.css";
 import TimeDate from "./TimeDate.js";
 import Forecast from "./Forecast";
 
-export default function Weather() {
-  const [city, setCity] = useState(null);
-  const [weatherInfo, setWeatherInfo] = useState({
-    name: "Berlin",
-    temperature: "16",
-  });
+export default function Weather(props) {
+  const [weatherInfo, setWeatherInfo] = useState({ loaded: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-  function showWeatherCondition(response) {
+  function handleResponse(response) {
     setWeatherInfo({
+      loaded: true,
       name: response.data.name,
-      temperature: Math.round(response.data.main.temp),
+      date: new Date(response.data.dt * 1000),
+      temperature: response.data.main.temp,
       description: response.data.weather[0].description,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
@@ -23,87 +22,96 @@ export default function Weather() {
     });
   }
 
+  function handleSearch() {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=caa34f2f02bff4b74600363cf67bbbef&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=caa34f2f02bff4b74600363cf67bbbef&units=metric`;
-    axios.get(apiUrl).then(showWeatherCondition);
+    handleSearch();
   }
 
   function getCity(event) {
     setCity(event.target.value);
   }
 
-  return (
-    <div className="Weather">
-      <div className="row">
-        <div className="col-5">
-          <h1>
-            <span className="temp" id="temperature">
-              {weatherInfo.temperature}
-            </span>
-            <span className="degrees">
-              <a href="/" className="font-day" id="celsius-temp">
-                °C
-              </a>{" "}
-              |{" "}
-              <a href="/" className="font-day" id="fahrenheit-temp">
-                °F
-              </a>
-            </span>
-          </h1>
-          <TimeDate />
-        </div>
-
-        <div className="col-7 p-0 city-condition">
-          <h2>
-            <span id="city-name">{weatherInfo.name}</span>
-          </h2>
-          <center>
-            <img src={weatherInfo.image} alt={weatherInfo.description} />
-          </center>
-          <h3>
-            <span id="description">Partly cloudy</span>
-            <br />
-            Wind: <span id="wind">4</span>km/h <br />
-            Humidity: <span id="humidity">80</span>%
-          </h3>
-        </div>
-      </div>
-
-      <form className="Search" onSubmit={handleSubmit}>
+  if (weatherInfo.loaded) {
+    return (
+      <div className="Weather">
         <div className="row">
-          <div className="col-5 pr-1">
-            <div className="form-group">
-              <input
-                type="search"
-                className="form-control"
-                placeholder="Enter your city"
-                autoComplete="off"
-                autoFocus="on"
-                onChange={getCity}
-              />
+          <div className="col-5">
+            <h1>
+              <span className="temp">
+                {Math.round(weatherInfo.temperature)}
+              </span>
+              <span className="degrees">
+                <a href="/" className="font-day" id="celsius-temp">
+                  °C
+                </a>{" "}
+                |{" "}
+                <a href="/" className="font-day" id="fahrenheit-temp">
+                  °F
+                </a>
+              </span>
+            </h1>
+            <TimeDate />
+          </div>
+
+          <div className="col-7 p-0 city-condition">
+            <h2>
+              <span>{weatherInfo.name.toUpperCase()}</span>
+            </h2>
+            <center>
+              <img src={weatherInfo.image} alt={weatherInfo.description} />
+            </center>
+            <h3>
+              <span>{weatherInfo.description}</span>
+              <br />
+              Wind: <span>{Math.round(weatherInfo.wind)}</span>km/h <br />
+              Humidity: <span>{weatherInfo.humidity}</span>%
+            </h3>
+          </div>
+        </div>
+
+        <form className="Search" onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-5 pr-1">
+              <div className="form-group">
+                <input
+                  type="search"
+                  className="form-control"
+                  placeholder="Enter your city"
+                  autoComplete="off"
+                  autoFocus="on"
+                  onChange={getCity}
+                />
+              </div>
+            </div>
+            <div className="col-3 pr-0 pl-1">
+              <button type="submit" className="btn btn-light">
+                Search
+              </button>
+            </div>
+            <div className="col-4 pl-1">
+              <button
+                type="button"
+                className="btn btn-light"
+                id="current-location"
+              >
+                <span role="img" aria-label="Location">
+                  📍
+                </span>{" "}
+                Location
+              </button>
             </div>
           </div>
-          <div className="col-3 pr-0 pl-1">
-            <button type="submit" className="btn btn-light">
-              Search
-            </button>
-          </div>
-          <div className="col-4 pl-1">
-            <button
-              type="button"
-              className="btn btn-light"
-              id="current-location"
-            >
-              <span role="img" aria-label="Location">
-                📍
-              </span>{" "}
-              Location
-            </button>
-          </div>
-        </div>
-      </form>
-      <Forecast />
-    </div>
-  );
+        </form>
+        <Forecast />
+      </div>
+    );
+  } else {
+    handleSearch();
+    return "Loading...";
+  }
 }
